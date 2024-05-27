662c2db57b248fce671a1d40a90fcdad0a01b285
luisfelipems <luisfelipe.martinssilva@gmail.com>
2024-05-27 01:22:22 +0000

    Apache versão 1.0

---
+++ script-iac2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Baixando atualização do Linux."
+apt-get update
+
+echo "Instalando atualizações do Linux."
+apt-get upgrade -y
+
+echo "Instalando o Apache2."
+apt-get install apache2 -y
+
+echo "Instalando o Unzip."
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação"
+ 
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
